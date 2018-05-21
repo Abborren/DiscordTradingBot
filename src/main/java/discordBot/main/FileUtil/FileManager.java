f729b289bde0749b920ea03792182e5327582526
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-21 22:42:59 +0200

    uncommented debug code

--- src/main/java/discordBot/main/FileUtil/FileManager.java
+++ src/main/java/discordBot/main/FileUtil/FileManager.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class FileManager {
     public BufferedImage load(File filename) {
         try {
-            System.out.println("Loading succeeded");
+            //System.out.println("Loading succeeded");
            return ImageIO.read(filename);
         } catch (IOException e) {
             System.out.println("Image Load Failed!");
