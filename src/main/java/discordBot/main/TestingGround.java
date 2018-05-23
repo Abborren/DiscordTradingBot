c0f7cc8201b88fd5b6d668e92b2dd03b216689fb
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-23 23:21:20 +0200

    added new testing ground code

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -4,7 +4,8 @@ import discordBot.main.outputWindow.ChannelManager;
 
 public class TestingGround {
     public static void main(String[] args) {
-        new ChannelManager().initiateTradingChannels();
-
+        ChannelManager channelManager = new ChannelManager();
+        channelManager.initiateTradingChannels();
+        channelManager.addItems("<:lacquerware:365925547563286528>",channelManager.getTradingChannelWithNameAndId("Balenos",1));
     }
 }
