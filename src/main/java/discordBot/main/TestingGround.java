d02bb0e55835c2799eb8a3536052d976f3ca54f5
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-23 22:23:31 +0200

    removed old testing stuff

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -1,23 +1,10 @@
 package discordBot.main;
 
-import discordBot.main.FileUtil.FileManager;
-import discordBot.main.FileUtil.image.Compare;
-import discordBot.main.FileUtil.image.CropImage;
-import discordBot.main.FileUtil.image.ImageLogic;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
+import discordBot.main.outputWindow.ChannelManager;
 
 public class TestingGround {
-    private static CropImage cropImage = new CropImage();
-    private static ImageLogic imageLogic = new ImageLogic();
-    private static Compare compare = new Compare();
-    private static FileManager fileManager = new FileManager();
     public static void main(String[] args) {
-        //BufferedImage inputImg = fileManager.load(new File("Images/Downloaded/Refs/ref2.png"));
-        //BufferedImage tradeImg = fileManager.load(new File("Images/Downloaded/Input/ref2.png"));
-        imageLogic.compareImageTest();
+        new ChannelManager().initiateTradingChannels();
 
-        //System.out.println(compare.findSubImageDouble(tradeImg,inputImg,0.01));
     }
 }
