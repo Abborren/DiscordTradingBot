a63a3db1d0e1e6b07debaa10f53de535022cce97
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-24 11:33:28 +0200

    commented stuff that needs App main

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -5,7 +5,7 @@ import discordBot.main.outputWindow.ChannelManager;
 public class TestingGround {
     public static void main(String[] args) {
         ChannelManager channelManager = new ChannelManager();
-        channelManager.initiateTradingChannels();
-        channelManager.addItems("<:lacquerware:365925547563286528>",channelManager.getTradingChannelWithNameAndId("Balenos",1));
+        //bachannelManager.initiateTradingChannels(main);
+        //channelManager.addItems("<:lacquerware:365925547563286528>",channelManager.getTradingChannelWithNameAndId("Balenos",1),null);
     }
 }
