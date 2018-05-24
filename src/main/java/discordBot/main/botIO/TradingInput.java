ac06cbc2779a5f05301b41ec351e2bac14f64c78
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-24 15:58:33 +0200

    added returnItems amountItems and canParse

---
+++ src/main/java/discordBot/main/botIO/TradingInput.java
@@ -0,0 +1,33 @@
+package discordBot.main.botIO;
+
+import java.util.ArrayList;
+
+public class TradingInput {
+    public String[] returnItems(String[] message) {
+        ArrayList<String> returnList = new ArrayList<String>();
+        for (String thisMessage : message) {
+            if (thisMessage.startsWith(":")) {
+                returnList.add(thisMessage);
+            }
+        }
+        return returnList.toArray(new String[0]);
+    }
+    public String[] returnAmount(String[] message) {
+        ArrayList<String> returnList = new ArrayList<String>();
+        for (String thisMessage : message) {
+            if (!thisMessage.startsWith(":") && canParse(thisMessage)) {
+                returnList.add(thisMessage);
+            }
+        }
+        return returnList.toArray(new String[0]);
+    }
+    private boolean canParse(String s) {
+        try {
+            int i = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+}
