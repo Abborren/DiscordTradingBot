ffddab9bdc7c446600713740dd09b771c15f114c
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-25 18:23:49 +0200

    returnItems now adds null to list if thisMessage is not a item

--- src/main/java/discordBot/main/botIO/TradingInput.java
+++ src/main/java/discordBot/main/botIO/TradingInput.java
@@ -8,6 +8,8 @@ public class TradingInput {
         for (String thisMessage : message) {
             if (thisMessage.startsWith("<:")) {
                 returnList.add(thisMessage);
+            } else {
+                returnList.add(null);
             }
         }
         return returnList.toArray(new String[0]);
