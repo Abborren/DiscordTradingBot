ed43f4fa2afea87bce90124321361d3636d5d7db
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-25 19:15:36 +0200

    added logic for removing items

--- src/main/java/discordBot/main/botIO/Commands.java
+++ src/main/java/discordBot/main/botIO/Commands.java
@@ -8,7 +8,6 @@ import discordBot.main.outputWindow.Window.PrintEmbed;
 import net.dv8tion.jda.core.entities.*;
 
 import java.io.File;
-import java.io.PrintStream;
 
 public class Commands {
     private ImageLogic imageLogic = new ImageLogic();
@@ -38,24 +37,56 @@ public class Commands {
 
     }
 
+    void removeItems(Message objMsg, MessageChannel objChannel, App main) {
+        if (objMsg.getContentRaw().contains(" ")) {
+            String[] inputMsg;
+            inputMsg = objMsg.getContentRaw().split(" ");
+            if (inputMsg[0].equals("!wipe")) {
+                String command = inputMsg[1];
+                String[] temp = objMsg.getContentRaw().substring(2).split(" ");
+                for (int i = 0; i < temp.length; i++) {
+                    temp[i] = temp[i].toLowerCase();
 
-    void tradingCommands(User objUser, Message objMsg, MessageChannel objChannel, App main) {
-        checkIfChannelsAreNeeded(main);
+                }
+                TradingInput tradingInput = new TradingInput();
+                String[] items = tradingInput.returnItems(temp);
+                String[] channelCommandsArray = {"ba", "ve", "se", "me", "ca", "va", "ka", "ar"};
+                int[] channelNumberArray = {1, 2, 3, 4, 5, 6};
+
+                outerLoop:
+                for (String channelCommand : channelCommandsArray) {
+                    for (int number : channelNumberArray) {
+                        //if channel limit is met it will break
+                        if (channelCommand.equals("ka") && number < 4 || channelCommand.equals("ar") && number < 1) {
+                            break outerLoop;
+                        } else if (command.startsWith(channelCommand) && command.endsWith(String.valueOf(number))) {
+                            TradingChannelObject tradingChannel = main.channelManager.getTradingChannelWithCallSignAndId(inputMsg[1].substring(0, 2), Integer.parseInt(String.valueOf(inputMsg[1].charAt(2))), main);
+                            for (int i = 0; i < items.length; i++) {
+                                tradingChannel.removeItem(items[i], null);
+                            }
+                            break outerLoop;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void printEmbed(MessageChannel objChannel,App main) {
+        PrintEmbed printEmbed = new PrintEmbed();
+        printEmbed.printEmbed(main,objChannel);
+    }
+    void addItems(User objUser, Message objMsg, MessageChannel objChannel, App main) {
 
         Attachments attachments = new Attachments();
-        if (objMsg.getContentRaw().equalsIgnoreCase("!PrintOut")) {
-            PrintEmbed printEmbed = new PrintEmbed();
-            printEmbed.printEmbed(main,objChannel);
-        }
-
         if (objMsg.getContentRaw().contains(" ")) {
             String[] inputCmdMsg;
             inputCmdMsg = objMsg.getContentRaw().split(" ");
 
             String callSign = inputCmdMsg[0];
-            String[] temp = objMsg.getContentRaw().substring(2).split(" ");
+            String[] temp = objMsg.getContentRaw().substring(4).split(" ");
             for (int i = 0; i < temp.length; i++) {
                 temp[i] = temp[i].toLowerCase();
+                System.out.println("input is"+temp[i]);
             }
             TradingInput tradingInput = new TradingInput();
             String[] items = tradingInput.returnItems(temp);
@@ -75,7 +106,8 @@ public class Commands {
                         } else {
                             TradingChannelObject tradingChannel = main.channelManager.getTradingChannelWithCallSignAndId(inputCmdMsg[0].substring(0, 2), Integer.parseInt(String.valueOf(inputCmdMsg[0].charAt(2))), main);
                             for (int i = 0; i < items.length; i++) {
-                                main.channelManager.addItem(items[i], tradingChannel, amount[i]);
+
+                                tradingChannel.addItem(items[i],amount[i]);
                             }
                         }
                         break outerLoop;
@@ -85,7 +117,7 @@ public class Commands {
             }
         }
     }
-    private void checkIfChannelsAreNeeded(App main){
+    void checkIfChannelsAreNeeded(App main){
         //creates trading channels
         if (main.tradingChannelObjects.isEmpty()) {
             main.channelManager.clearTradingChannels(main);
