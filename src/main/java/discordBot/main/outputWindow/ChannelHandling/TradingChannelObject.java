56baa1dd8f94564ca2c61cdd53d0689302f16444
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-25 17:56:52 +0200

    mode item adding and removing into the channel object

--- src/main/java/discordBot/main/outputWindow/ChannelHandling/TradingChannelObject.java
+++ src/main/java/discordBot/main/outputWindow/ChannelHandling/TradingChannelObject.java
@@ -1,5 +1,7 @@
 package discordBot.main.outputWindow.ChannelHandling;
 
+import java.util.Arrays;
+
 public class TradingChannelObject {
     public String name;
     public int id;
@@ -15,5 +17,52 @@ public class TradingChannelObject {
         for (int i= 0; i < items.length; i++) {
             items[i][1] = "N/A";
         }
+    }
+
+    public void removeItem(String tradingItem, String amount) {
+        String[][] itemPairs = {{"<:lacquerware:365925547563286528>","<:cencer:365926188968968222>"},
+                {"<:lamp:365926091241816076>","<:ginseng:365926221122371586>"},
+                {"<:spice:365926064116989972>","<:slab:365926150561726465>"},
+                {"<:saber:365926042180911114>","<:porcelain:365925475630972928>"},
+                {"<:kite:365926019254714369>","<:silk:365926117024202754>"}};
+        for (int i = 0; i < itemPairs.length; i++) {
+
+            if (itemPairs[i][0].equalsIgnoreCase(tradingItem) || itemPairs[i][1].equalsIgnoreCase(tradingItem)) {
+                items[((i+1)*2)-2][0] = null;
+                items[((i+1)*2)-1][0] = null;
+                if (!items[i][1].equals("N/A")) {
+                    items[i][1] = "N/A";
+                }
+                break;
+            }
+        }
+        System.out.println("Channel now contains "+ Arrays.deepToString(items));
+    }
+
+    public void addItem(String tradingItem, String amount) {
+        String[][] itemPairs = {{"<:lacquerware:365925547563286528>","<:cencer:365926188968968222>"},
+                {"<:lamp:365926091241816076>","<:ginseng:365926221122371586>"},
+                {"<:spice:365926064116989972>","<:slab:365926150561726465>"},
+                {"<:saber:365926042180911114>","<:porcelain:365925475630972928>"},
+                {"<:kite:365926019254714369>","<:silk:365926117024202754>"}};
+        for (int i = 0; i < itemPairs.length; i++) {
+
+               if (itemPairs[i][0].equalsIgnoreCase(tradingItem)) {
+                    items[((i+1)*2)-2][0] = itemPairs[i][0];
+                    items[((i+1)*2)-1][0] = itemPairs[i][1];
+                    if (!amount.equals("N/A")) {
+                        items[i][1] = amount;
+                    }
+                    break;
+               } else if (itemPairs[i][1].equalsIgnoreCase(tradingItem)) {
+                   items[((i+1)*2)-1][0] = itemPairs[i][0];
+                   items[((i+1)*2)-2][0] = itemPairs[i][1];
+                   if (!amount.equals("N/A")) {
+                       items[i+1][1] = amount;
+                   }
+                   break;
+               }
+        }
+        System.out.println("Channel now contains "+ Arrays.deepToString(items));
     }
 }
