8dd99531c96a54d2062d2d5fe225a4a7d6e20c7b
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 11:35:03 +0200

    added trading reset logic

--- src/main/java/discordBot/main/botTime/TimeObj.java
+++ src/main/java/discordBot/main/botTime/TimeObj.java
@@ -1,23 +1,42 @@
 package discordBot.main.botTime;
 
 import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Date;
-
 public class TimeObj implements Runnable {
     @Override
     public void run() {
-        Date date;
+
         long interval = 1000;
         long previousMillis =0;
+        LocalDateTime[] resetTime = new LocalDateTime[6];
+        int[] resets = {0, 4, 8, 12, 16, 20};
+        for (int i = 0; i < resets.length;i++) {
+            resetTime[i] = LocalDateTime.of(LocalDate.now(),LocalTime.of(resets[i],0));
+        }
+
         while (true) {
             long currentMillis = System.currentTimeMillis();
-            LocalTime timeUTC = LocalTime.now(Clock.systemUTC());
+            LocalDateTime timeUTC = LocalDateTime.now(Clock.systemUTC());
             if (currentMillis - previousMillis >= interval) {
                 previousMillis = currentMillis;
-
-                System.out.println(timeUTC.getHour()+":"+timeUTC.getMinute()+":"+timeUTC.getSecond()); // 06:08:18.125
+                for (int i = 0; i < resetTime.length; i++) {
+                    if (!checkTradingReset(timeUTC, resetTime[i]).equals(resetTime[i])) {
+                        resetTime[i] = resetTime[i].plusDays(1);
+                    }
+                }
+                //System.out.println(timeUTC.getHour()+":"+timeUTC.getMinute()+":"+timeUTC.getSecond()); // hour:minute:second
             }
         }
+    }
+    private LocalDateTime checkTradingReset(LocalDateTime timeUTC, LocalDateTime resetTime) {
+
+        if (timeUTC.isAfter(resetTime)) {
+            System.out.println("trading resets");
+
+            return resetTime.plusDays(1);
+        }
+        return resetTime;
     }
 }
