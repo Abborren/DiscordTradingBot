9dbac5af35999ef46d0a631f7972d7331cebd556
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 13:28:47 +0200

    added a GuildHandler object, messageChannels array and print Embed object

--- src/main/java/discordBot/main/Bot.java
+++ src/main/java/discordBot/main/Bot.java
@@ -3,9 +3,11 @@
 package discordBot.main;
 import javax.security.auth.login.LoginException;
 
+import discordBot.main.botIO.input.GuildHandler;
 import discordBot.main.botIO.input.MessageReceived;
 import discordBot.main.botIO.output.ChannelHandling.ChannelManager;
 import discordBot.main.botIO.output.ChannelHandling.TradingChannelObject;
+import discordBot.main.botIO.output.Window.PrintEmbed;
 import discordBot.main.botTime.TimeObj;
 import discordBot.tokenUtil.TokenUtil;
 import net.dv8tion.jda.core.AccountType;
@@ -16,12 +18,15 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.util.ArrayList;
+import java.util.logging.Handler;
 
 public class Bot extends ListenerAdapter {
     public static ArrayList<TextChannel> textChannels = new ArrayList<TextChannel>();
     public  ChannelManager channelManager = new ChannelManager();
     public  ArrayList<TradingChannelObject> tradingChannelObjects = new ArrayList<TradingChannelObject>();
-
+    public PrintEmbed printEmbed = new PrintEmbed();
+    public GuildHandler guildHandler = new GuildHandler();
+    public MessageChannel[] messageChannels;
     public static void main(String[] args) throws LoginException, InterruptedException {
         new Bot();
     }
@@ -31,16 +36,13 @@ public class Bot extends ListenerAdapter {
         //Initializes the bot
         JDA jdaBot = new JDABuilder(AccountType.BOT).setToken(tokenUtil.loadToken()).buildBlocking();
         jdaBot.addEventListener(this);
-        jdaBot.getPresence().setGame(Game.of(Game.GameType.DEFAULT ,"testing testing beep boop"));
         textChannels.addAll(jdaBot.getTextChannels());
-        TimeObj timeObj = new TimeObj(this);
+        TimeObj timeObj = new TimeObj(this,jdaBot);
         Thread timeThread = new Thread(timeObj);
         timeThread.start();
-
     }
     @Override
     public void onMessageReceived(MessageReceivedEvent messageEvent) {
         new MessageReceived(messageEvent,this).messageReceivedHandler();
     }
-
 }
