c9e7605f7bfa319335b217768eada95c86d83155
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 12:29:12 +0200

    renamed from Handler to GuildHandler

--- src/main/java/discordBot/main/botIO/input/GuildHandler.java
+++ src/main/java/discordBot/main/botIO/input/GuildHandler.java
@@ -5,11 +5,12 @@ import net.dv8tion.jda.core.entities.Role;
 
 import java.util.List;
 
-class Handler {
+class GuildHandler {
     boolean checkChannel(MessageChannel channel, String channelName) {
         // Checks if command is written in specific channel.
         return channel.getName().equalsIgnoreCase(channelName);
     }
+    MessageChannel getChannel()
     boolean checkRole(List<Role> roles, String s) {
         //compares all the roles of the user to check if it has required role
         for(Role role : roles) {
