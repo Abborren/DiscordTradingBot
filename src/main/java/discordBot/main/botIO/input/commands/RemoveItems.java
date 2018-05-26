68c6f6cfdc22ef04f90e314767891baf992965e9
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 23:29:26 +0200

    put logic in its own thread

--- src/main/java/discordBot/main/botIO/input/commands/RemoveItems.java
+++ src/main/java/discordBot/main/botIO/input/commands/RemoveItems.java
@@ -1,47 +1,50 @@
 package discordBot.main.botIO.input.commands;
 
 import discordBot.main.Bot;
-import discordBot.main.botIO.input.TradingInput;
 import discordBot.main.botIO.output.ChannelHandling.TradingChannelObject;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 
 public class RemoveItems {
     public void removeItems(Message objMsg, MessageChannel objChannel, Bot main) {
-        boolean removeAll = true;
-        if (objMsg.getContentRaw().startsWith("!wipe") && objMsg.getContentRaw().contains(" ")) {
+        Thread thread = new Thread(() -> {
 
-            String[] temp = objMsg.getContentRaw().substring(6).split(" ");
-            for (int i = 0; i < temp.length; i++) {
-                temp[i] = temp[i].toLowerCase();
+            boolean removeAll = true;
+            if (objMsg.getContentRaw().startsWith("!wipe") && objMsg.getContentRaw().contains(" ")) {
 
-            }
-            for (String s: temp) {
-                if (s.startsWith("<:")) {
-                    removeAll = false;
+                String[] temp = objMsg.getContentRaw().substring(6).split(" ");
+                for (int i = 0; i < temp.length; i++) {
+                    temp[i] = temp[i].toLowerCase();
+
                 }
-            }
-            TradingInput tradingInput = new TradingInput();
-            String[] items = tradingInput.returnRemoveItems(temp,removeAll);
-            String[] channelCommandsArray = {"ba", "ve", "se", "me", "ca", "va", "ka", "ar"};
-            int[] channelNumberArray = {1, 2, 3, 4, 5, 6};
-
-            outerLoop:
-            for (String channelCommand : channelCommandsArray) {
-                for (int number : channelNumberArray) {
-                    //if channel limit is met it will break
-                    if (channelCommand.equals("ka") && number < 4 || channelCommand.equals("ar") && number < 1) {
-                        break outerLoop;
-                    } else if (temp[0].startsWith(channelCommand) && temp[0].endsWith(String.valueOf(number))) {
-                        TradingChannelObject tradingChannel = main.channelManager.getTradingChannelWithCallSignAndId(temp[0].substring(0, 2), Integer.parseInt(String.valueOf(temp[0].charAt(2))), main);
-                        for (String item : items) {
-                            tradingChannel.removeItem(item);
-                        }
-                        break outerLoop;
+                for (String s : temp) {
+                    if (s.startsWith("<:")) {
+                        removeAll = false;
                     }
                 }
+                TradingInput tradingInput = new TradingInput();
+                String[] items = tradingInput.returnRemoveItems(temp, removeAll);
+                String[] channelCommandsArray = {"ba", "ve", "se", "me", "ca", "va", "ka", "ar"};
+                int[] channelNumberArray = {1, 2, 3, 4, 5, 6};
+
+                outerLoop:
+                for (String channelCommand : channelCommandsArray) {
+                    for (int number : channelNumberArray) {
+                        //if channel limit is met it will break
+                        if (channelCommand.equals("ka") && number < 4 || channelCommand.equals("ar") && number < 1) {
+                            break outerLoop;
+                        } else if (temp[0].startsWith(channelCommand) && temp[0].endsWith(String.valueOf(number))) {
+                            TradingChannelObject tradingChannel = main.channelManager.getTradingChannelWithCallSignAndId(temp[0].substring(0, 2), Integer.parseInt(String.valueOf(temp[0].charAt(2))), main);
+                            for (String item : items) {
+                                tradingChannel.removeItem(item);
+                            }
+                            break outerLoop;
+                        }
+                    }
+                }
+                main.printEmbed.editEmbed(main, objChannel);
             }
-            main.printEmbed.editEmbed(main,objChannel);
-        }
+        });
+        thread.start();
     }
 }
