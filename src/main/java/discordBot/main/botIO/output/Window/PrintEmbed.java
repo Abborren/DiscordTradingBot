758126c88585ef1f173186cdbeb2f3dfcb456e12
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 18:27:27 +0200

    printEmbed now returns the messageId of the trading window

--- src/main/java/discordBot/main/botIO/output/Window/PrintEmbed.java
+++ src/main/java/discordBot/main/botIO/output/Window/PrintEmbed.java
@@ -1,11 +1,19 @@
 package discordBot.main.botIO.output.Window;
 
 import discordBot.main.Bot;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 
 public class PrintEmbed {
-    public void printEmbed(Bot main, MessageChannel channel) {
+    public String printEmbed(Bot main, MessageChannel channel) {
         BuildEmbed buildEmbed = new BuildEmbed();
-        channel.sendMessage(buildEmbed.buildOutput(main).build()).queue();
+        MessageEmbed message = buildEmbed.buildOutput(main).build();
+        return channel.sendMessage(message).complete().getId();
+    }
+    public void editEmbed(Bot main, MessageChannel channel) {
+        BuildEmbed buildEmbed = new BuildEmbed();
+        channel.editMessageById(main.botMessage,buildEmbed.buildOutput(main).build()).complete();
     }
 }
