ec051e7c9c4ebbd2d566831d4cdc9e526590f68c
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-26 12:52:28 +0200

    added getMessageChannels it gives all message channels of all servers the bot is connected to

--- src/main/java/discordBot/main/botIO/input/GuildHandler.java
+++ src/main/java/discordBot/main/botIO/input/GuildHandler.java
@@ -1,16 +1,27 @@
 package discordBot.main.botIO.input;
 
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.Role;
 
+import java.util.ArrayList;
 import java.util.List;
 
-class GuildHandler {
-    boolean checkChannel(MessageChannel channel, String channelName) {
+public class GuildHandler {
+    public boolean checkChannel(MessageChannel channel, String channelName) {
         // Checks if command is written in specific channel.
         return channel.getName().equalsIgnoreCase(channelName);
     }
-    MessageChannel getChannel()
+    public MessageChannel[] getMessageChannels(JDA jda) {
+        ArrayList<MessageChannel> channels = new ArrayList<>();
+        for(Guild g : jda.getGuilds()) {
+            for(MessageChannel c : g.getTextChannels()) {
+                channels.add(c);
+            }
+        }
+        return channels.toArray(new MessageChannel[0]);
+    }
     boolean checkRole(List<Role> roles, String s) {
         //compares all the roles of the user to check if it has required role
         for(Role role : roles) {
