299aa416188bc3749e36fba2925949eabf87edee
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-27 15:50:53 +0200

    Created Discord user object to track of active roll

---
+++ src/main/java/discordBot/bot/botTime/discordUser/DiscordUser.java
@@ -0,0 +1,15 @@
+package discordBot.bot.botTime.discordUser;
+
+import net.dv8tion.jda.core.entities.User;
+
+import java.time.LocalDateTime;
+
+public class DiscordUser {
+    public LocalDateTime timeUTC;
+    public User user;
+    public DiscordUser(LocalDateTime timeUTC, User user) {
+        this.user = user;
+        this.timeUTC = timeUTC.plusHours(16);
+        //determineTime(timeUTC);
+    }
+}
