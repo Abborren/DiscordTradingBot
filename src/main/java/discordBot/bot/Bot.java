9c9f585ff2d874888cee05f5be782708c41e06a7
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-27 15:49:14 +0200

    Created DiscordUsers arrayList

--- src/main/java/discordBot/bot/Bot.java
+++ src/main/java/discordBot/bot/Bot.java
@@ -6,18 +6,19 @@ import javax.security.auth.login.LoginException;
 import discordBot.bot.botIO.input.MessageReceived;
 import discordBot.bot.botIO.output.ChannelHandling.TradingChannelObject;
 import discordBot.bot.botTime.TimeObj;
+import discordBot.bot.botTime.discordUser.DiscordUser;
 import discordBot.tokenUtil.TokenUtil;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.util.ArrayList;
 
 public class Bot extends ListenerAdapter {
-    public  ArrayList<TradingChannelObject> tradingChannelObjects = new ArrayList<TradingChannelObject>();
+    public ArrayList<TradingChannelObject> tradingChannelObjects = new ArrayList<TradingChannelObject>();
+    public ArrayList<DiscordUser> discordUsers = new ArrayList<>();
     public String botMessageId;
     private JDA jdaBot;
     public static void main(String[] args) throws LoginException, InterruptedException {
