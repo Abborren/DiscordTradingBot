1682a7d22f420951ec87a2a9c7529b480ea19f8a
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-27 15:46:32 +0200

    active command now adds user to DiscordObject list with a time inactive command removes user from DiscordObject list

--- src/main/java/discordBot/bot/botIO/input/commands/TradingRoles.java
+++ src/main/java/discordBot/bot/botIO/input/commands/TradingRoles.java
@@ -1,15 +1,19 @@
 package discordBot.bot.botIO.input.commands;
 
+import discordBot.bot.Bot;
 import discordBot.bot.botIO.output.tempMessages.TempMessage;
+import discordBot.bot.botTime.discordUser.DiscordUser;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.managers.GuildController;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class TradingRoles {
-    public void giveRole(MessageChannel channel, Message message, User user, List<Role> userRoles, JDA jdaBot) {
+    public void giveRole(MessageChannel channel, Message message, User user, List<Role> userRoles, JDA jdaBot,Bot main) {
         Thread thread = new Thread(() -> {
             Guild guild = channel.getJDA().getGuilds().get(1);
             GuildController guildController =new GuildController(guild);
@@ -22,6 +26,13 @@ public class TradingRoles {
                     if (userRoles.contains(roles.get(0))) {
                         guildController.removeRolesFromMember(guild.getMember(user),roles.get(0)).complete();
                         tempMessage.printTempMessage(channel,new MessageBuilder(user.getAsMention()+", you are now inactive!"),6000);
+
+                        for (int i = 0; i < main.discordUsers.size(); i++) {
+                            if (main.discordUsers.get(i).user.getId().equalsIgnoreCase(user.getId())) {
+                                main.discordUsers.remove(main.discordUsers.get(i));
+                                break;
+                            }
+                        }
                     } else {
                         tempMessage.printTempMessage(channel,new MessageBuilder(user.getAsMention()+", you are already inactive"),6000);
                     }
@@ -29,6 +40,7 @@ public class TradingRoles {
                 } else if (messageS.endsWith("active")) {
                     if (!userRoles.contains(roles.get(0))) {
                         guildController.addRolesToMember(guild.getMember(user),roles.get(0)).complete();
+                        main.discordUsers.add(new DiscordUser(LocalDateTime.now(Clock.systemUTC()),user));
                         tempMessage.printTempMessage(channel,new MessageBuilder(user.getAsMention()+", you are now active!"),6000);
 
                     } else {
