6c1822f17beb2162bb6c4ed9ff5078c57d810e55
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-27 15:00:27 +0200

    deleted testing ground since it no longer serves a purpose

--- src/main/java/discordBot/bot/TestingGround.java
+++
@@ -1,9 +0,0 @@
-package discordBot.bot;
-
-public class TestingGround {
-    public static void main(String[] args) {
-        //ChannelManager channelManager = new ChannelManager();
-        //bachannelManager.initiateTradingChannels(bot);
-        //channelManager.addItem("<:lacquerware:365925547563286528>",channelManager.getTradingChannelWithNameAndId("Balenos",1),null);
-    }
-}
